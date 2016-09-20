b2065b6d7dc2a91610e932607f8ba2a4ef1b8b58
ltakiguc <ltakiguc@uci.edu>
2016-09-19 19:29:58 -0700

    Grade Average updating

--- script.js
+++ script.js
@@ -38,7 +38,13 @@ function addStudent(){
     var name = $("#studentName").val();
     var course = $("#course").val();
     var grade = $("#studentGrade").val();
-    student_array.push({student: name, course: course, grade: grade});
+    var student = {student: name, course: course, grade: grade};
+    student_array.push(student);
+
+    var average = calculateAverage(); // ** TODO: reallocate this action to another part of the code
+    $("div div small span").html(average);
+    addStudentToDom(student);
+    //TODO: Display student and average
     return undefined; //** QUESTION: ??? Why?
 }
 /**
@@ -52,29 +58,54 @@ function addStudent(){
 function calculateAverage(){
     var sum = 0; // ** Holds the total of all the grades added together
     for(var index in student_array){
-        sum += student_array[index].grade;
+        sum += Number(student_array[index].grade);
     }
     return sum / student_array.length;
 }
 /**
  * updateData - centralized function to update the average and call student list update
  */
-
+function updateData(){
+    calculateAverage();
+    updateStudentList();
+}
 /**
  * updateStudentList - loops through global student array and appends each objects data into the student-list-container > list-body
  */
-
+function updateStudentList(){
+    //**TODO updateStudentList
+}
 /**
  * addStudentToDom - take in a student object, create html elements from the values and then append the elements
  * into the .student_list tbody
  * @param studentObj
  */
+function addStudentToDom(studentObj){
+    // var content = "<table>";
+    // content += "<tr><td>" + studentObj.student + "</td></tr>";
+    // // for(var i = 0; i < student_array.length; i++){
+    // //     content += "<tr><td>" + student_array[i].student +  i + "</td></tr>";
+    // // }
+    // content += "</table>";
+    //
+    // $('.student_list tbody').append(content);
 
+
+    //** Take 3:
+    // var table = $('<table></table>');
+    // var content = "<tr><td>" + studentObj.student + "</td></tr>";
+    // table.append(content);
+    // $('.student_list tbody').append(table);
+}
 /**
  * reset - resets the application to initial state. Global variables reset, DOM get reset to initial load state
  */
-
-
+function reset(){
+    student_array = [];
+    inputIds = [];
+    //TODO: reset inputs
+    //TODO: reset dom
+}
 /**
  * Listen for the document to load and reset the data to the initial state
  */
