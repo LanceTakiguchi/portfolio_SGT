e01887d5b81895683c2a5e99ece2e62cb65be6ca
ltakiguc <ltakiguc@uci.edu>
2017-02-22 13:23:29 -0800

    Code cleaned. Removed old in progress code. Deleted firebase related code. Deleted extra js file. Deleted loading animation. Deleted logs

--- js/script.js
+++ js/script.js
@@ -1,44 +1,40 @@
 /**
  * AngularJS main app holding all controllers
  */
- var app = angular.module("sgt_app", []);
- // loading for each http request
- app.config(function ($httpProvider) {
-  
+ var app = angular.module("sgt_app");
+// loading for each http request
+app.config(function ($httpProvider) {
 })
- /**
+/**
   * Service that holds all shared data between Angular controllers
   */
   app.service("shared_data", function(){
-
-  /**
-   * Holds the local array of all students
-   * @type Array An array holding student objects that hold student's info
-   */
-   this.all_students = [];
-  /**
-   * Variable holding the grade average
-   * @type Number Represents the grade average
-   */
-   this.grade_average = 0;
+    var shared = this;
+    this.all_students = [];
+    this.id_count = -1;
+    this.id_counter = function() {
+      this.id_count++;
+      return this.id_count;
+    };
+    this.grade_average = 0;
   /**
    * Takes the grades from all the students and returns the average
    * @return String || Number Returns an empty string if there is no array to average, else it returns a Number average
    */
    this.calculate_grade_average = function(){
     if(this.all_students.length === 0){
-      return ""; //** Display nothing, as there is no average
+      this.grade_average = "N/A";
+      return this.grade_average; //** Display nothing, as there is no average
     }
-            var sum = 0; // ** Holds the total of all the grades added together
-            //** Get the sum of all the grades
-            for(var index in this.all_students){
-              sum += Number(this.all_students[index].grade);
-            }
-            //** Divide the sum by the number of students
-            this.grade_average = Math.round(sum / this.all_students.length);
-            return this.grade_average;
-          };
-          
+    var sum = 0; // ** Holds the total of all the grades added together
+    //** Get the sum of all the grades
+    for(var index in this.all_students){
+      sum += Number(this.all_students[index].grade);
+    }
+    //** Divide the sum by the number of students
+    this.grade_average = Math.round(sum / this.all_students.length);
+    return this.grade_average;
+  };
     /**
      * Adds student into the all_student's array
      * @param Object student An Object with the student's name, course, grade, and id
@@ -76,7 +72,7 @@
      this.delete_student = function(id){
       for(student_index in this.all_students){
         if(this.all_students[student_index].id === id){
-          this.all_students.splice(student_index, 0);
+          this.all_students.splice(student_index, 1);
           return true;
         }
       }
@@ -96,9 +92,9 @@
       }
       return false;
     };
-  })
+})
 /** controller that just calculates grade average */
-app.controller("app_controller", function($log, shared_data) {
+app.controller("app_controller", function(shared_data) {
   this.grade_average = shared_data.calculate_grade_average();
   /** Grabs the service's calculated grade avereage */
   this.get_grade_average = function(){
@@ -106,44 +102,44 @@ app.controller("app_controller", function($log, shared_data) {
   }
 });
 /**
- * Controller for the intial student data grab from the server and deleting students
+ * Controller for the UX inputs, clear functionality, asking server to add student, and form input validation
  */
- app.controller("table_controller", function($http, $log, shared_data){
-
-
-});
-/**
- * Controller for the UX inputs, clear functionality, and asking server to add student
- */
- app.controller("form_controller", function($log, shared_data) {
-    //form_controller
-    this.display = function(type){
-      if(type === "title"){
-        return shared_data.current_display().display;  
-      }
-      return shared_data.current_display().display_details
-    }
+ app.controller("form_controller", function(shared_data) {
+  this.input_name = "";
+  this.input_course = "";
+  this.input_grade = "";
+  this.all_students = [];
+  /**
+   * Clear DOM inputs
+   */
+   this.clear_inputs = function(){
     this.input_name = "";
     this.input_course = "";
     this.input_grade = "";
-    this.all_students = [];
-    this.clear_inputs = function(message){
-      this.input_name = "";
-      this.input_course = "";
-      this.input_grade = "";
-      if(message){
-        shared_data.message("clear");
-        shared_data.remove_red();
-      }
-    }
-
-  });
-
- $(document).ready(function() {
-    $('.commentarea').keydown(function(event) {
-        if (event.keyCode == 13) {
-            this.form.submit();
-            return false;
-         }
-    });
+  }
+  /**
+   * Take inputed student and add it into the shared_data
+   */
+   this.new_student = function() {
+    var new_student = {};
+    new_student.name = this.input_name;
+    new_student.course = this.input_course;
+    new_student.grade = this.input_grade;
+    new_student.id = shared_data.id_counter();
+    shared_data.add_student(new_student);
+    this.clear_inputs();
+  }
+});
+ /**
+ * Controller for the displaying students on DOM
+ */
+ app.controller("table_controller", function($http, shared_data){
+  this.students = shared_data.return_students();
+  /**
+   * Deletes student from shared_data
+   * @param  Object student The student that is trying to be deleted
+   */
+   this.invoke_delete = function(student) {
+    shared_data.delete_student(student.id);
+  };
 });
